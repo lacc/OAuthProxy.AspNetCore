7ed62cf095a5f4265ea4373f3563837b3abab180
Laszlo <mornailla@gmail.com>
2025-07-12 00:02:42 +0200

    clear usings

--- src/OAuthProxy.AspNetCore/Services/RefreshTokenService.cs
+++ src/OAuthProxy.AspNetCore/Services/RefreshTokenService.cs
@@ -2,11 +2,6 @@
 using OAuthProxy.AspNetCore.Abstractions;
 using OAuthProxy.AspNetCore.Configurations;
 using OAuthProxy.AspNetCore.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OAuthProxy.AspNetCore.Services
 {
