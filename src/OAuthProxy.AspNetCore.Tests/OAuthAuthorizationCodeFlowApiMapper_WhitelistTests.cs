87cb917edcabb7147d544b9cd8cf5b28e4ca9788
Laszlo <mornailla@gmail.com>
2025-07-13 23:25:59 +0200

    adding unit test

---
+++ src/OAuthProxy.AspNetCore.Tests/OAuthAuthorizationCodeFlowApiMapper_WhitelistTests.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using OAuthProxy.AspNetCore.Apis;
+using Xunit;
+
+namespace OAuthProxy.AspNetCore.Tests
+{
+    public class OAuthAuthorizationCodeFlowApiMapper_WhitelistTests
+    {
+        [Theory]
+        [InlineData(new string[0], "https://any.url/allowed", true)] // Empty whitelist allows all
+        [InlineData(new[] { "https://allowed.com/callback" }, "https://allowed.com/callback", true)]
+        [InlineData(new[] { "https://allowed.com/callback" }, "https://notallowed.com/callback", false)]
+        [InlineData(new[] { "https://allowed.com/*" }, "https://allowed.com/callback", true)]
+        [InlineData(new[] { "https://allowed.com/*" }, "https://allowed.com/other", true)]
+        [InlineData(new[] { "https://allowed.com/*" }, "https://allowed.com/", true)]
+        [InlineData(new[] { "https://allowed.com/*" }, "https://notallowed.com/callback", false)]
+        [InlineData(new[] { "https://allowed.com/callback", "https://other.com/*" }, "https://other.com/abc", true)]
+        [InlineData(new[] { "https://allowed.com/callback", "https://other.com/*" }, "https://allowed.com/callback", true)]
+        [InlineData(new[] { "https://allowed.com/callback", "https://other.com/*" }, "https://notallowed.com/", false)]
+        [InlineData(new[] { "https://allowed.com/callback*" }, "https://allowed.com/callback", true)]
+        [InlineData(new[] { "https://allowed.com/callback*" }, "https://allowed.com/callbackExtra", true)]
+        [InlineData(new[] { "https://allowed.com/callback*" }, "https://allowed.com/call", false)]
+        [InlineData(new[] { "https://allowed.com/callback" }, "https://allowed.com/callback/", false)]
+        public void IsUrlWhitelisted_WorksAsExpected(string[] whitelist, string redirectUrl, bool expected)
+        {
+            var result = InvokeIsUrlWhitelisted(whitelist, redirectUrl);
+            Assert.Equal(expected, result);
+        }
+
+        private static bool InvokeIsUrlWhitelisted(IEnumerable<string> whitelist, string redirectUrl)
+        {
+            // Use reflection to call the private static method
+            var method = typeof(OAuthAuthorizationCodeFlowApiMapper)
+                .GetMethod("IsUrlWhitelisted", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            return (bool)method.Invoke(null, new object[] { whitelist, redirectUrl });
+        }
+    }
+}
